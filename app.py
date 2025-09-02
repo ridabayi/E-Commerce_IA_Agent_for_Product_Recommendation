b8a708d7201be14f306ab0248fae062243402bb9
ridabayi <bayi.rida@gmail.com>
2025-09-02 15:48:17 +0200

    update app

--- app.py
+++ app.py
@@ -195,6 +195,4 @@ def create_app() -> Flask:
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(
-        host=os.getenv("FLASK_HOST", "0.0.0.0"), port=int(os.getenv("PORT", "5000")), debug=True
-    )
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", "5000")), debug=True)
